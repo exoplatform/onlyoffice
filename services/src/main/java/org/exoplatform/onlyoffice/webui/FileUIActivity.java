c2b42fd6230c325fa178083cfebe544244ad34b0
Ali HAMDI <ahamdi@exoplatform.com>
2020-06-29 16:17:51 +0100

    MAINT-24415 : add missing Download action in FileUIActivity component (#58)

--- services/src/main/java/org/exoplatform/onlyoffice/webui/FileUIActivity.java
+++ services/src/main/java/org/exoplatform/onlyoffice/webui/FileUIActivity.java
@@ -57,7 +57,9 @@ import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
         @EventConfig(listeners = BaseUIActivity.DeleteCommentActionListener.class),
         @EventConfig(listeners = BaseUIActivity.LikeCommentActionListener.class),
         @EventConfig(listeners = BaseUIActivity.EditActivityActionListener.class),
-        @EventConfig(listeners = BaseUIActivity.EditCommentActionListener.class) }), })
+        @EventConfig(listeners = BaseUIActivity.EditCommentActionListener.class),
+        @EventConfig(listeners = FileUIActivity.DownloadDocumentActionListener.class)})
+})
 public class FileUIActivity extends org.exoplatform.wcm.ext.component.activity.FileUIActivity {
 
   /** The Constant LOG. */
