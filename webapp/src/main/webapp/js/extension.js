f322f08fb4bfc2b08c4dad0ff1606e7fe65953dd
Anas Elbenney <42932906+aelbenney@users.noreply.github.com>
2021-10-13 14:37:55 +0100

    TASK-47206: Implement new Document extension (#132)
    This will add a new portlet that contains different type of documents that can be created. It will be used in attachment drawer in order to create and link new documents.

---
+++ webapp/src/main/webapp/js/extension.js
@@ -0,0 +1,34 @@
+(() => {
+  const newDocumentTypeExtensionOptions = [
+    {
+      id: 'docs',
+      rank: 10,
+      label: 'documents.type.docs',
+      extension: '.docx',
+      type: 'MicrosoftOfficeDocument',
+      icon: 'uiIconFileTypeapplicationvndopenxmlformats-officedocumentwordprocessingmldocument uiIconFileTypeDefault'
+    },
+    {
+      id: 'sheets',
+      rank: 20,
+      label: 'documents.type.sheets',
+      extension: '.xlsx',
+      type: 'MicrosoftOfficeSpreadsheet',
+      icon: 'uiIconFileTypeapplicationvndopenxmlformats-officedocumentspreadsheetmlsheet uiIconFileTypeDefault'
+    },
+    {
+      id: 'slides',
+      rank: 30,
+      label: 'documents.type.slides',
+      extension: '.pptx',
+      type: 'MicrosoftOfficePresentation',
+      icon: 'uiIconFileTypeapplicationvndopenxmlformats-officedocumentpresentationmlpresentation uiIconFileTypeDefault'
+    }
+  ];
+  const lang = eXo.env.portal.language || 'en';
+  const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.navigation.portal.global-${lang}.json`;
+
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => new Vue({i18n}));
+  newDocumentTypeExtensionOptions.forEach(extension => extensionRegistry.registerExtension('attachment', 'new-document-action', extension));
+  document.dispatchEvent(new CustomEvent('attachment-new-document-action-updated'));
+})();
