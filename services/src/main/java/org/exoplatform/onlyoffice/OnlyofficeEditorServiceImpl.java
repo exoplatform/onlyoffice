be0574b272843190784f324291f9b43785bc219a
MayTekayaa <mtekaya@exoplatform.com>
2026-02-16 12:38:23 +0100

    EXO-82012 Change the canAccessDocumentLocation variable after checking the document location permission

--- services/src/main/java/org/exoplatform/onlyoffice/OnlyofficeEditorServiceImpl.java
+++ services/src/main/java/org/exoplatform/onlyoffice/OnlyofficeEditorServiceImpl.java
@@ -705,13 +705,7 @@ public class OnlyofficeEditorServiceImpl implements OnlyofficeEditorService, Sta
           String ecmsPageLink = explorerLink(path);
           builder.explorerUri(explorerUri(schema, host, port, ecmsPageLink));
           builder.secret(documentserverSecret);
-          boolean canAccessDocument = false;
-          if (path.startsWith(usersPath)) {
-            canAccessDocument = canEditDocument(node);
-          } else {
-            canAccessDocument = true;
-          }
-          builder.canAccess(canAccessDocument);
+          builder.canAccessDocumentLocation(canAccessDocumentLocation(path, userId));
 
           config = builder.build();
 
@@ -735,11 +729,7 @@ public class OnlyofficeEditorServiceImpl implements OnlyofficeEditorService, Sta
       config.getEditorPage().setComment(nodeComment(node));
       config.getEditorPage().setLastModifier(getLastModifier(node));
       config.getEditorPage().setLastModified(getLastModified(node));
-      if (path.startsWith(usersPath)) {
-        config.getEditorConfig().setCanAccess(canEditDocument(node));
-      } else {
-        config.getEditorConfig().setCanAccess(true);
-      }
+      config.getEditorConfig().setCanAccessDocumentLocation(canAccessDocumentLocation(path, userId));
 
       cachedEditorConfigStorage.saveConfig(config.getDocument().getKey(), config,false);
       cachedEditorConfigStorage.saveConfig(config.getDocId(),config,false);
@@ -831,14 +821,7 @@ public class OnlyofficeEditorServiceImpl implements OnlyofficeEditorService, Sta
     } else {
       builder.setAllowEdition(false);
     }
-
-    boolean canAccessDocument = false;
-    if (path.startsWith(usersPath)) {
-      canAccessDocument = canEditDocument(node);
-    } else {
-      canAccessDocument = true;
-    }
-    builder.canAccess(canAccessDocument);
+    builder.canAccessDocumentLocation(canAccessDocumentLocation(path, userId));
 
     Config config = builder.build();
     // Create users' config map and add first user
@@ -1225,6 +1208,29 @@ public class OnlyofficeEditorServiceImpl implements OnlyofficeEditorService, Sta
     }
     return getPages(versions, itemParPage, pageNum);
   }
+
+  private String extractSpacePrettyName(String path) {
+    if (path == null) {
+      return null;
+    }
+    String[] parts = path.split("/");
+
+    if (parts.length > 3 && "Groups".equals(parts[1]) && "spaces".equals(parts[2])) {
+      return parts[3];
+    }
+    return null;
+  }
+  private boolean canAccessDocumentLocation(String path, String userId) {
+    if (path.startsWith(usersPath)) {
+        return path.contains(userId);
+    }
+    if (path.startsWith(groupsPath)) {
+        String spaceName = extractSpacePrettyName(path);
+        Space space = spaceService.getSpaceByPrettyName(spaceName);
+        return space != null && spaceService.isMember(space, userId);
+    }
+    return false;
+}
 
   private <T> List<T> getPages(List<T> c, Integer pageSize, int nb) {
     if (c == null || c.isEmpty())
